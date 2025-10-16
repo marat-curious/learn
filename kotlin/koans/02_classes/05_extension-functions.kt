c53cceb0d244ce4d5c8122417995834798b04593
Marat Amirov <marat.curious@gmail.com>
2025-10-16 10:00:00 +0300

    add "classes" section from koans

---
+++ kotlin/koans/02_classes/05_extension-functions.kt
@@ -0,0 +1,10 @@
+fun Int.r(): RationalNumber = RationalNumber(this, 1)
+
+fun Pair<Int, Int>.r(): RationalNumber = RationalNumber(this.first, this.second)
+
+data class RationalNumber(val numerator: Int, val denominator: Int)
+
+fun main() {
+    println(42.r())
+    println(Pair(1, 2).r())
+}
